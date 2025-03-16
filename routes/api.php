15823be44b0bfc00b08e089a9c11c79ed8fc0513
tonguiino <santiagotq2@gmail.com>
2025-03-16 15:02:02 -0500

    add routes for apis

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuariosController;
+use App\Http\Controllers\ServiciosController;
+use App\Http\Controllers\FacturasController;
+use App\Http\Controllers\CalificacionesController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('usuarios', UsuariosController::class);
+Route::apiResource('servicios', ServiciosController::class);
+Route::apiResource('facturas', FacturasController::class);
+Route::apiResource('calificaciones', CalificacionesController::class);
