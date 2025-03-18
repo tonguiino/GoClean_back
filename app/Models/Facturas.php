188890488c83525fda730431e2c6c6285ea2ac01
Cristian Molina <cristianmolinab121@gmail.com>
2025-03-18 16:32:17 -0500

    Implement CRUD operations for Facturas with validation and relationships

--- app/Models/Facturas.php
+++ app/Models/Facturas.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Facturas extends Model
 {
-    //
+    protected $table = 'facturas';
+
+    protected $fillable = [
+        'id_servicio', 'id_usuario', 'id_socio',
+        'monto', 'metodo_pago', 'detalles', 'fecha_pago'
+    ];
+
+    public function servicio()//relacion de uno a muchos
+    {
+        return $this->belongsTo(Servicios::class, 'id_servicio');
+    }
+
+    public function usuario()//relacion de uno a muchos
+    {
+        return $this->belongsTo(Usuarios::class, 'id_usuario');
+    }
+
+    public function socio()//relacion de uno a muchos
+    {
+        return $this->belongsTo(Usuarios::class, 'id_socio');
+    }
+
 }
