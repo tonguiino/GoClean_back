5e679b33117863aeb45cd7be1f5375f2e0565b0a
tonguiino <santiagotq2@gmail.com>
2025-03-20 11:59:32 -0500

    Mejora de codigo para index

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -12,12 +12,22 @@ class UsuariosController extends Controller
      */
     public function index()
     {
-        //
-        $usuarios = Usuarios::all();
-        return response()-> json([
-            'status' => 'succes',
-            'data' => $usuarios 
-        ],200);
+        // Este bloque de codigo contiene la logica para el metodo index a demas de tener manejo de errores
+        try {
+            $usuarios = Usuarios::all();
+            return response()->json([
+                'status' => 'succes',
+                'data' => $usuarios
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error al obtener ususarios = ',
+                'message' =>  $th->getMessage(), //Esta linea nos indicara el mensaje del cual origina el error
+                'line' => $th->getLine(), //Esta linea nos dice en que linea exacta sucedio el error
+                'code' => $th->getCode() //Esta linea nos dice el codigo del error si es 404 o 500 o el que sea 
+            ]);
+        }
     }
 
     /**
