9290917708c88fba13d1c75782a771c9060e02f6
tonguiino <santiagotq2@gmail.com>
2025-03-25 16:34:14 -0500

    add metodos store, show

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Usuarios;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use PhpParser\Node\Stmt\TryCatch;
 
 class UsuariosController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         // Este bloque de codigo contiene la logica para el metodo index a demas de tener manejo de errores
@@ -22,7 +21,47 @@ class UsuariosController extends Controller
         } catch (\Throwable $th) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Error al obtener ususarios = ',
+                'message' => 'Error al obtener ususarios',
+                'message' =>  $th->getMessage(), //Esta linea nos indicara el mensaje del cual origina el error
+                'line' => $th->getLine(), //Esta linea nos dice en que linea exacta sucedio el error
+                'code' => $th->getCode() //Esta linea nos dice el codigo del error si es 404 o 500 o el que sea 
+            ], 404);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nombre' => 'required|string|max:100',
+            'correo' => 'required|string|email|max:100|unique:usuarios,correo', //Si al utilizar el meotdo actualziar 'update' este nos genera algun error debemos poner 'except,id' en update!
+            'telefono' => 'required|string|min:10|max:18|unique:usuarios,telefono',
+            'direccion' => 'required|string|max:100',
+            'contrasena' => 'required|string|min:8|max:50|confirmed',
+            'sexo' => 'string|in:Masculino,Femenino,Otro',
+            'rol' => 'required|string|in:Cliente,Socio',
+            'verificado' => 'boolean'
+        ]);
+
+        try {
+            $usuarios = Usuarios::create([
+                'nombre' => $request->nombre,
+                'correo' => $request->correo,
+                'telefono' => $request->telefono,
+                'direccion' => $request->direccion,
+                'contrasena' => Hash::make($request->contrasena),
+                'sexo' => $request->sexo,
+                'rol' => $request->rol,
+                'verificado' => $request->verificado
+            ]);
+
+            return response()->json([
+                'status' => 'Ok',
+                'data' => $usuarios
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error al agregar ususarios',
                 'message' =>  $th->getMessage(), //Esta linea nos indicara el mensaje del cual origina el error
                 'line' => $th->getLine(), //Esta linea nos dice en que linea exacta sucedio el error
                 'code' => $th->getCode() //Esta linea nos dice el codigo del error si es 404 o 500 o el que sea 
@@ -30,20 +69,22 @@ class UsuariosController extends Controller
         }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function show(Usuarios $usuario)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Usuarios $usuarios)
-    {
-        //
+        try {
+            return response()->json([
+                'status' => 'ok',
+                'data' => $usuario
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error al mostrar ususario',
+                'message' =>  $th->getMessage(), //Esta linea nos indicara el mensaje del cual origina el error
+                'line' => $th->getLine(), //Esta linea nos dice en que linea exacta sucedio el error
+                'code' => $th->getCode() //Esta linea nos dice el codigo del error si es 404 o 500 o el que sea 
+            ]);
+        }
     }
 
     /**
