11f4c6ef4d2409e2952a64187e0417710fdd7751
tonguiino <santiagotq2@gmail.com>
2025-03-25 17:05:51 -0500

    finalizacion de todos los metodos para usuarios

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -65,7 +65,7 @@ class UsuariosController extends Controller
                 'message' =>  $th->getMessage(), //Esta linea nos indicara el mensaje del cual origina el error
                 'line' => $th->getLine(), //Esta linea nos dice en que linea exacta sucedio el error
                 'code' => $th->getCode() //Esta linea nos dice el codigo del error si es 404 o 500 o el que sea 
-            ]);
+            ], 500);
         }
     }
 
@@ -87,19 +87,54 @@ class UsuariosController extends Controller
         }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Usuarios $usuarios)
+
+    public function update(Request $request, Usuarios $usuario)
     {
-        //
+
+        try {
+            $request->validate([
+                'nombre' => 'required|string|max:100',
+                'correo' => 'required|string|email|max:100|unique:usuarios,correo,' . $usuario->id,
+                'telefono' => 'required|string|min:10|max:15|unique:usuarios,telefono,' . $usuario->id,
+                'direccion' => 'nullable|string|max:255',
+                'sexo' => 'nullable|string|in:Masculino,Femenino,Otro',
+                'rol' => 'required|string|in:Cliente,Socio',
+                'verificado' => 'boolean',
+            ]);
+
+            $usuario->update($request->all());
+
+            return response()->json([
+                'status' => 'ok',
+                'message' => 'Usuario actualizado correctamente',
+                'data' => $usuario
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error al actualizar usuario',
+                'debug' => $th->getMessage()
+            ], 500);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Usuarios $usuarios)
+    public function destroy(Usuarios $usuario)
     {
-        //
+        try {
+            $usuario->delete();
+            return response()->json([
+                'status' => 'ok',
+                'message' => 'Usuario borrado correctamente'
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error al eliminar usuario',
+                'debug' => $th->getMessage()
+            ], 500);
+        }
     }
 }
