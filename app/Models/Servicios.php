b21e82d09358a880ab04de38044f7ae01cdf5991
pablogil <pablogil25@hotmail.com>
2025-03-25 16:56:35 -0500

    refactizacion de crud sservicios, con try cath

--- app/Models/Servicios.php
+++ app/Models/Servicios.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Servicios extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'servicios';
+
+    protected $fillable = [
+        'usuario_id',
+        'socio_id',
+        'descripcion',
+        'estado'
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuarios::class, 'usuario_id');
+    }
+
+    public function socio()
+    {
+        return $this->belongsTo(Usuarios::class, 'socio_id');
+    }
 }
